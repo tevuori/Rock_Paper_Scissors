a8442a1c628c2fabd5460b5927d9baea8563b51b
tevuori <kuba.horak00754@gmail.com>
2022-04-21 20:26:22 +0200

    Completed project.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,60 @@
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        List<String> options = new ArrayList();
+        options.add("Paper");
+        options.add("Rock");
+        options.add("Scissors");
+        Boolean gameovver = false;
+        Scanner input = new Scanner(System.in);
+
+        while(gameovver==false){
+            System.out.println("Choose from these options:");
+            System.out.println("Paper / Rock / Scissors");
+            String choosed = input.next();
+
+            Random r = new Random();
+            int randomitem = r.nextInt(options.size());
+            String randompick = options.get(randomitem);
+            if(choosed.contains("Paper") || choosed.contains("Rock") || choosed.contains("Scissors")){
+                switch (choosed){
+                    case "Paper":
+                        if(Objects.equals(randompick, "Rock")){
+                            TimeUnit.SECONDS.sleep(1);
+                            System.out.println("Game Over! Computer picked Rock!");
+                        }
+                        if(Objects.equals(randompick, "Scissors")){
+                            TimeUnit.SECONDS.sleep(1);
+                            System.out.println("Congratulations! YOU WON!");
+                        }
+                        break;
+                    case "Rock":
+                        if(Objects.equals(randompick, "Scissors")){
+                            TimeUnit.SECONDS.sleep(1);
+                            System.out.println("Game Over! Computer picked Scissors!");
+                        }
+                        if(Objects.equals(randompick, "Paper")){
+                            TimeUnit.SECONDS.sleep(1);
+                            System.out.println("Congratulations! YOU WON!");
+                        }
+                        break;
+                    case "Scissors":
+                        if(Objects.equals(randompick, "Rock")){
+                            TimeUnit.SECONDS.sleep(1);
+                            System.out.println("Game Over! Computer picked Rock!");
+                        }
+                        if(Objects.equals(randompick, "Paper")){
+                            TimeUnit.SECONDS.sleep(1);
+                            System.out.println("Congratulations! YOU WON!");
+                        }
+                        break;
+                }
+                gameovver = true;
+            }else{
+                System.out.println("Invalid option, Try again!");
+            }
+        }
+    }
 }
